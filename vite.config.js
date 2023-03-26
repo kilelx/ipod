51706075ee46bbb9070993d1363aa67026a53518
kilelx <llx.kieran@gmail.com>
2023-03-26 17:28:27 +0200

    First commit

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
-// import { autoprefixer } from 'autoprefixer'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://github.com/kilelx/ipod.git",
   plugins: [react()],
-  // css: {
-  //   postcss: {
-  //     plugins: [autoprefixer],
-  //   },
-  // },
 })
